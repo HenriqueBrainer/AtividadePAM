a5c01fa5b0f210b48a478f705af36b87fdb8fae5
vini <vinisuuares!989@gmail.com>
2022-04-06 09:24:04 -0300

    Att de Pam

---
+++ PrimeiraAtividade/PrimeiraAtividade/PrimeiraAtividade/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PrimeiraAtividade
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+
+        private void btnContagem_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = "Você clicou " + count.ToString() + "Vezes";
+        }
+
+        private async Task btnLimpar_ClickedAsync(object sender, EventArgs e)
+        {
+            if(await DisplayAlert("Pergunta", "Deseja realmente Limpar?", "Yes", "No"))
+            {
+                btnContagem.Text = "Clique Aqui";
+                count = 0;
+            }
+        }
+    }
+}
